fd19a4ac3f51442a7992f64cb8f6ac482026717a
Connor van Spronssen <info@dobefu.com>
2026-01-04 12:07:50 +0100

    feat: Add a method to get a subscription

---
+++ get_subscription.go
@@ -0,0 +1,31 @@
+package mollie
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// GetSubscription gets a single subscription.
+func (c *Client) GetSubscription(
+	customerID,
+	subscriptionID string,
+) (*Subscription, error) {
+	_, respBodyJSON, err := c.request(
+		"GET",
+		fmt.Sprintf("/customers/%s/subscriptions/%s", customerID, subscriptionID),
+		nil,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var respBody Subscription
+	err = json.Unmarshal(respBodyJSON, &respBody)
+
+	if err != nil {
+		return nil, fmt.Errorf("could not unmarshal response body: %s", err.Error())
+	}
+
+	return &respBody, nil
+}
